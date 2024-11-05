b01816059d1698affceeb8f531ea7cbdcd1034d8
seiya_tagami <bepozurapu274@gmail.com>
2024-11-05 14:50:12 +0900

    testing

---
+++ .github/actions/echo/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# `$#` expands to the number of arguments and `$@` expands to the supplied `args`
+printf "Running scripts with %d arguments: %s\n" "$#" "$@"
+deno run --allow-env main.ts "$@"
