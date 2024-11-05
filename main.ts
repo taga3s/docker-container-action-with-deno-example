cd669b00cbde9353de09500f2fcde136816bd65a
seiya_tagami <bepozurapu274@gmail.com>
2024-11-05 14:44:25 +0900

    testing

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 const SECRET_KEY = Deno.env.get('SECRET_KEY') ?? ''
-// const MESSAGE = Deno.args[0] ?? ''
+const MESSAGE = Deno.args[0] ?? ''
 
 function main() {
   if (SECRET_KEY === "secret_key") {
-    console.log("TEST2")
+    console.log(MESSAGE)
   }
 }
 
